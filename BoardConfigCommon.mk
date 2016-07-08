f6f886b0f926000ba5fc10d56a52210b7e571420
Brandon McAnsh <brandon.mcansh@gmail.com>
2016-07-08 00:55:48 -0400

    zero: Enable Cortex-A53 erratum
    Change-Id: I1292f91e5e3de88edef822b6c873b3489eccc45f
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -34,6 +34,9 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
+# A53 Erratum 
+TARGET_CPU_CORTEX_A53 := true
+
 ENABLE_CPUSETS := true
 
 # Binder

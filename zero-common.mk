5dcbf6b3a1d86232c7e3305c2a1df2c2da7f9a06
Brandon McAnsh <brandon.mcansh@gmail.com>
2016-08-19 14:03:19 -0400

    zero: Pull common properties out of makefile
    Change-Id: Ife7a8e0461af62d40d75175726c9cb6f29f2fb74
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- zero-common.mk
+++ zero-common.mk
@@ -157,17 +157,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     power.universal7420
 
-# common build properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=640 \
-    ro.opengles.version=196609 \
-    ro.chipname=exynos7420 \
-    af.fast_track_multiplier=1 \
-    audio_hal.force_voice_config=wide \
-    ro.nfc.sec_hal=true \
-    wifi.interface=wlan0 \
-    debug.hwc.force_gpu=1
-
 # Radio
 PRODUCT_PACKAGES += \
     libxml2 \
